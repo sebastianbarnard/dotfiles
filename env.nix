e67754f2c1d183a895a43bb4c48dd976e811b36c
Gareth Hughes <gareth.hughes@nagra.com>
2024-04-05 07:52:04 +0100

    Update environment configuration and remove unused dependencies

--- env.nix
+++ env.nix
@@ -1,19 +1,5 @@
-{ pkgs ? import <nixpkgs> { } }:
-let
-  inherit (pkgs) vscode-with-extensions vscodium;
-
-  vscodiumWithExtensions = vscode-with-extensions.override {
-    vscode = vscodium;
-    vscodeExtensions = with pkgs.vscode-marketplace; [
-      catppuccin.catppuccin-vsc-icons
-      dracula-theme.theme-dracula
-      jnoortheen.nix-ide
-      mkhl.direnv
-      rust-lang.rust-analyzer
-      humao.rest-client
-    ];
-  };
-in
+{ pkgs ? import <nixpkgs> { }
+}:
 
 pkgs.buildEnv {
   name = "my dev environment";
--- flake.nix
+++ flake.nix
@@ -2,25 +2,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nix-vscode-extensions = {
-      url = "github:nix-community/nix-vscode-extensions";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
   };
 
-  outputs = inputs:
+  outputs = {nixpkgs, ...} @ inputs:
     inputs.flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = inputs.nixpkgs.legacyPackages.${system}.extend inputs.nix-vscode-extensions.overlays.default;
-
+          pkgs = import nixpkgs { inherit system; };
+          
           myEnv = import ./env.nix {
             inherit pkgs;
           };
         in
         {
-          packages.default = myEnv;
+          packages = {
+            inherit myEnv;
+            default = myEnv;
+          };
         }
       );
 }
