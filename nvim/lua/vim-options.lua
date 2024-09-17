5b2293544f49dd7aa030a35c2f20b637ff5223dc
Gareth Hughes <gareth.hughes@nagra.com>
2024-09-17 07:49:56 +0100

    Settings for obsidian

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,4 +1,5 @@
 vim.cmd("set relativenumber")
+vim.cmd("set number")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -9,3 +10,5 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+
+vim.opt.conceallevel = 1
