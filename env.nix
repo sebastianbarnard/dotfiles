ba56f4fb5f3b8b4cb09be120bcd6141cac573184
Seb Barnard <seb.barnard@btinternet.com>
2024-09-23 18:43:50 +0100

    Updated nix file

--- env.nix
+++ env.nix
@@ -5,25 +5,17 @@ pkgs.buildEnv {
   name = "my dev environment";
   paths = with pkgs; [
     awscli2
-    colima
     direnv
     docker
-    fd
-    gcc
     git
-    httpie
     k9s
     kitty
     kubectl
     kubernetes-helm
-    lazygit
-    neovim
     nixpkgs-fmt
-    nodejs_20 
+    fnm
     python3
-    qemu
-    ripgrep
-    starship
     terraform
+    warp-terminal
   ];
 }
