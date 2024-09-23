b245f9819d17c80aae7dfc0629ff2410ddf74fb3
sebastianbarnard <sebastian.barnard@nagra.com>
2024-09-23 20:08:10 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, ... } @ inputs:
     inputs.flake-utils.lib.eachDefaultSystem
       (system:
@@ -12,24 +11,25 @@
             inherit system;
             config.allowUnfree = true;
           };
-
           myEnv = import ./env.nix {
             inherit pkgs;
           };
+          warpShell = import ./warp-shell.nix;
         in
         {
           packages = {
             inherit myEnv;
             default = myEnv;
           };
-
-          devShells.default = pkgs.mkShell {
-            name = "dotfiles";
-
-            packages = with pkgs; [
-              nixpkgs-fmt
-              stylua
-            ];
+          devShells = {
+            default = pkgs.mkShell {
+              name = "dotfiles";
+              packages = with pkgs; [
+                nixpkgs-fmt
+                stylua
+              ];
+            };
+            warp = warpShell;
           };
         }
       );
