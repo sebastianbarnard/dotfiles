6875148002d83daf7955db357a02d053ae84fba8
sebastianbarnard <sebastian.barnard@nagra.com>
2024-09-23 19:48:40 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -16,29 +16,10 @@
           myEnv = import ./env.nix {
             inherit pkgs;
           };
-
-          starshipConfig = pkgs.writeText "starship.toml" (builtins.readFile ./starship.toml);
-          nvimConfig = pkgs.runCommand "nvim-config" { } ''
-            mkdir -p $out
-            cp -r ${./nvim}/* $out
-          '';
-
-          applyConfig = pkgs.writeShellScriptBin "apply-config" ''
-            echo "Applying configuration..."
-
-            ln -sf ${starshipConfig} ~/.config/starship.toml
-
-            mkdir -pv ~/.config/nvim/lua/plugins
-            ln -sf ${nvimConfig}/init.lua ~/.config/nvim/init.lua
-            ln -sf ${nvimConfig}/lua/vim-options.lua ~/.config/nvim/lua/vim-options.lua
-            for file in ${nvimConfig}/lua/plugins/*; do
-              ln -sf "$file" ~/.config/nvim/lua/plugins/
-            done
-          '';
         in
         {
           packages = {
-            inherit myEnv nvimConfig applyConfig;
+            inherit myEnv;
             default = myEnv;
           };
 
